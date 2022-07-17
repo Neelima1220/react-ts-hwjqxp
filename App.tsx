3862d41d531e83d8c5172472f575a14973fd3094
Neelima1220 <neelimareddy1220@gmail.com>
2022-07-17 16:30:03 +0300

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,27 +1,53 @@
 import React, { useState } from 'react';
 import './style.css';
+import { cloneDeep } from 'lodash';
 import { Data } from './data';
 
 export default function App() {
-  const [selected, setSelected] = useState(Data[0]);
-  const [increment, setIncrement] = useState();
+  const [dataT, setDataT] = useState(Data);
+  // const [cur, setCur] = useState(0);
+  const [selected, setSelected] = useState();
+  console.log(selected, 'selected');
+
+  const handleClick = (idx) => {
+    const tempData = cloneDeep(dataT);
+    const n = tempData.map((item) => {
+      if (item.id === idx) {
+        return { ...item, age: item.age + 1 };
+      } else {
+        return item;
+      }
+    });
+    setDataT(n);
+  };
+
+  const handleSelected = (idx) => {
+    console.log(idx);
+    setSelected(dataT[idx - 1]);
+  };
   return (
     <div>
-      <h1>Selected user : {`${selected.name} is ${selected.age} years old`}</h1>
-      {Data.map((item, key) => (
-        <div
-          style={{
-            display: 'flex',
-            alignitems: 'center',
-            justifyContent: 'space-between',
-            margin: '1rem',
-          }}
-        >
-          <p> {`${item.name} is ${item.age} years old`}</p>
-          <button onClick={() => setIncrement(item.age + 1)}>Increment </button>
-          <button>Selected </button>
-        </div>
-      ))}
+      <h1>
+        Selected user :{' '}
+        {`${selected?.name || dataT[0].name} is ${
+          selected?.age || dataT[0].age
+        } years old`}
+      </h1>
+      {dataT &&
+        dataT.map((item, index) => (
+          <div
+            style={{
+              display: 'flex',
+              alignitems: 'center',
+              justifyContent: 'space-between',
+              margin: '1rem',
+            }}
+          >
+            <p> {`${item.name} is ${item.age} years old`}</p>
+            <button onClick={() => handleClick(item.id)}>Increment </button>
+            <button onClick={() => handleSelected(item.id)}>Selected </button>
+          </div>
+        ))}
     </div>
   );
 }
